f42addaebde6566589b79a4125eaccefb66d30a1
abauruel <abauruel@gmail.com>
2020-05-03 13:26:52 -0300

    added new routes

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,19 @@ const multerConfig = require('./config/multer');
 const upload = multer(multerConfig);
 
 const CompanyController = require('./app/controllers/companyController');
+const CategoryController = require('./app/controllers/categoryController');
+const PaymentMethod = require('./app/controllers/paymentMethodController');
 
 routes.post('/company', CompanyController.store);
 routes.get('/company', CompanyController.index);
 routes.get('/company/:id', CompanyController.show);
 routes.put('/company/:id', CompanyController.update);
 routes.delete('/company/:id', CompanyController.delete);
+
+routes.post('/category', CategoryController.store);
+routes.get('/category', CategoryController.index);
+
+routes.post('/payment_method', PaymentMethod.store);
+routes.get('/payment_method', PaymentMethod.index);
 
 module.exports = routes;
