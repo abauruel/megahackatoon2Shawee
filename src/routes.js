d821b03729b89e6b2ba0d53b4be717b318a7ec1b
facundo prieto <facundo.prieto321@gmail.com>
2020-05-03 01:27:09 -0300

    Clean routes tests

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,3 @@
 const routes = require('express').Router();
-const Company = require('./app/models/Company');
-const Category = require('./app/models/Category');
-const Payment_method = require('./app/models/Payment_method');
-const Budget = require('./app/models/Budget');
-const Category_company = require('./app/models/Category_company');
-const Payment_Company = require('./app/models/Payment_Company');
-const User = require('./app/models/User');
-
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'test',
-    phone: 1128254840,
-    email: 'test@gmail.com',
-    password: '123456789',
-  });
-  return res.json(user);
-});
 
 module.exports = routes;
