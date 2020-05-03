bcdbbba628ee730682352035be7f00fddb2e1ef5
abauruel <abauruel@gmail.com>
2020-05-03 13:44:15 -0300

    added method comment controller

--- src/app/controllers/commentController.js
+++ src/app/controllers/commentController.js
@@ -1,9 +1,21 @@
+const Comment = require('../models/Comment');
+
 module.exports = {
-  async show(req, res) {
+  async index(req, res) {
     return res.json({ ok: true });
   },
   async store(req, res) {
-    return res.json({ ok: true });
+    const { company_id, user_id, rate, comment } = req.body;
+    if (!company_id && !user_id)
+      return res.status(400).json({ message: 'Company or user invalid' });
+    const commentary = await Comment.create({
+      company_id,
+      user_id,
+      rate,
+      comment,
+    });
+
+    return res.json(commentary);
   },
   async update(req, res) {
     return res.status(200).send();
