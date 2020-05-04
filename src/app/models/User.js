1c869246c925c48db59b56f5ecac023dc93fbaa4
abauruel <abauruel@gmail.com>
2020-05-04 01:48:57 -0300

    added hook config

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 class User extends Sequelize.Model {
   static init(sequelize) {
@@ -7,17 +8,28 @@ class User extends Sequelize.Model {
         name: Sequelize.STRING,
         phone: Sequelize.INTEGER,
         email: Sequelize.STRING,
-        password: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
     return this;
   }
 
   static associate(models) {
     this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
