1fe8ccd5ca1ccd0ae075d3c4fae9b02c1748183a
abauruel <abauruel@gmail.com>
2020-05-04 19:48:00 -0300

    added features bulkcreate

--- src/app/controllers/fileController.js
+++ src/app/controllers/fileController.js
@@ -2,12 +2,9 @@ const File = require('../models/File');
 
 module.exports = {
   async store(req, res) {
-    const { location: url, key, originalname: name } = req.file;
-    const file = await File.create({
-      name,
-      key,
-      url,
-    });
-    return res.json(file);
+    const { files } = req;
+    const filesCreated = await File.bulkCreate(files);
+
+    return res.json(filesCreated);
   },
 };
